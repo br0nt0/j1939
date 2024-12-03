3e4ff86aac0f2391b06f5c5f0dca0b215acbbca7
br0nt0 <babilon5000@gmail.com>
2024-12-04 00:14:22 +0200

    #5 wrap up the receive CAN message and decode to j1939 format methods

--- src/j1939/messageReceive.c
+++ src/j1939/messageReceive.c
@@ -1,10 +1,12 @@
 /*******************************************************************************
  * @file	messageReceive.c
- * @brief	
+ * @brief
  * @author	@br0nt0
  * @date	2024
  ******************************************************************************/
 #include "messageReceive.h"
+#include "messageConverter.h"
+
 /******************************************************************************/
 
 /******************************************************************************/
@@ -12,7 +14,19 @@
 /******************************************************************************/
 j1939Message_t receiveJ1939MessageFromCANDriver( canDriver_t driver )
 {
-    ( void ) driver;
-    return ( NULL );
+    static j1939MessageStruct_t j1939MessageMemory;
+    j1939Message_t j1939Message = NULL;
+
+    if ( driver != NULL )
+    {
+        canMessage_t canMessage = receiveCANMessage( driver );
+        if ( canMessage != NULL )
+        {
+            j1939Message = &j1939MessageMemory;
+            decodeMessage( j1939Message, canMessage );
+        }
+    }
+
+    return ( j1939Message );
 }
 
