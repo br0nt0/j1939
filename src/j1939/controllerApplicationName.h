354a6f6ce4bd80b573b78d2a3f853fcf979e6c81
br0nt0 <babilon5000@gmail.com>
2024-12-11 23:05:18 +0200

    #8 set up controller application NAME

---
+++ src/j1939/controllerApplicationName.h
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * @file	controllerApplicationName.h
+ * @brief	
+ * @author	@br0nt0
+ * @date	2024
+ ******************************************************************************/
+#ifndef CONTROLLERAPPLICATIONNAME_H_
+#define CONTROLLERAPPLICATIONNAME_H_
+/******************************************************************************/
+#include "commonTypes.h"
+
+/******************************************************************************/
+const uint8_t* getControllerApplicationName( void );
+void setControllerapplicationName( const uint8_t* name );
+
+#endif /* CONTROLLERAPPLICATIONNAME_H_ */
---
+++ tests/j1939/addressClaimedTests.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * @file	addressClaimedTests.cpp
+ * @brief	
+ * @author	@br0nt0
+ * @date	2024
+ ******************************************************************************/
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+extern "C"
+{
+}
+
+TEST_GROUP( addressClaimed )
+{
+    void setup( void )
+    {
+    }
+    void teardown( void )
+    {
+    }
+};
+
+TEST( addressClaimed, given_null_then_no_seg_fault )
+{
+    // given
+
+    // when
+
+    // then
+    FAIL("Start here!!!");
+}
