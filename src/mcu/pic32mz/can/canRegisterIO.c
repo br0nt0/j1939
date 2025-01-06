63d84cfa88c900c5f1341ddc03852876884d49a4
br0nt0 <babilon5000@gmail.com>
2025-01-06 18:48:30 +0200

    #11 implement actual HW access for CiFIFOUA

--- src/mcu/pic32mz/can/canRegisterIO.c
+++ src/mcu/pic32mz/can/canRegisterIO.c
@@ -10,17 +10,40 @@
 #define CAN2_BASE_ADDRESS 0xBF881000u
 
 /******************************************************************************/
+static const canRegisters_t* canBase[ CAN_NUMBER_OF_MODULES ] =
+{
+    ( canRegisters_t* ) CAN1_BASE_ADDRESS,
+    ( canRegisters_t* ) CAN2_BASE_ADDRESS
+};
 
+
+static canRxMessageBuffer_t* address2RxBuffer( uintptr_t address )
+{
+	return ( ( canRxMessageBuffer_t* ) address );
+}
+
+static canTxMessageBuffer_t* address2TxBuffer( uintptr_t address )
+{
+	return ( ( canTxMessageBuffer_t* ) address );
+}
+
+static uint32_t pa2kva1( uint32_t address )
+{
+	return ( address | 0xa0000000u );
+}
 
 /******************************************************************************/
 canRegisters_t* getCANModuleRegisters( canModule_t module )
-{
-    static const canRegisters_t* canBase[ CAN_NUMBER_OF_MODULES ] =
-    {
-        ( canRegisters_t* ) CAN1_BASE_ADDRESS,
-        ( canRegisters_t* ) CAN2_BASE_ADDRESS
-    };
-    
+{    
     return ( ( canRegisters_t* ) canBase[ ( uint8_t ) module ] );
 }
 
+canRxMessageBuffer_t* getRxCxFIFOUA( canModule_t module, canFifo_t fifo )
+{
+	return ( address2RxBuffer( pa2kva1( canBase[ module ]->canFifoRegisters[ fifo ].CxFIFOUA ) ) );
+}
+
+canTxMessageBuffer_t* getTxCxFIFOUA( canModule_t module, canFifo_t fifo )
+{
+	return ( address2TxBuffer( pa2kva1( canBase[ module ]->canFifoRegisters[ fifo ].CxFIFOUA ) ) );
+}
